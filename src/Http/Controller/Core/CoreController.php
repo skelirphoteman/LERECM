b49cb1a46a578c0d538285f1986338471b8d7fc0
Skelir - bureau <galoin.julien@gmail.com>
2021-06-07 14:48:31 +0200

    [add] login form ok

--- src/Http/Controller/Core/CoreController.php
+++ src/Http/Controller/Core/CoreController.php
@@ -19,26 +19,6 @@ class CoreController extends AbstractController
      */
     public function core(): Response
     {
-        return new Response('ok');
+        return $this->render('core/index.html.twig');
     }
-
-    /**
-     * @Route("/create", name="test")
-     */
-    public function create(UserPasswordEncoderInterface $encoder) : Response
-    {
-        $entityManager = $this->getDoctrine()->getManager();
-
-
-        $apiconnection = new ApiAdminConnection();
-        $apiconnection->setName("skelir_panel");
-        $token = "dz4%?rXqPjp/E<J!Nm-,3^;B`VMgWh{n52}(SDt7wKx_[C)F*HpJ!%a@~j-6zgG<m8usAdFbKe#`27*n.}SQhUBy3^?c4E[/D:9Y";
-        $apiconnection->setPasswordToken(password_hash($token, PASSWORD_BCRYPT));
-
-        $entityManager->persist($apiconnection);
-
-        $entityManager->flush();
-        return new Response('ok');
-    }
-
 }
